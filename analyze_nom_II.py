ecf778efda64d7405558c54e6137e63f2d996e79
ebeling <ebeling@physik.uni-kiel.de>
2023-03-03 20:10:08 +0100

    Änderung

--- analyze_nom_II.py
+++ analyze_nom_II.py
@@ -234,59 +234,58 @@ class STEP:
                         tmp = ax[i].pcolormesh(ptime,ebins,(pdat/vmax).T, cmap = cmap, vmin = np.amin(1/vmax)*0.99,vmax = 1.)
                     
                     
-                if (norm and i == 0): # or not norm:
-                    tax = fig.add_subplot(4,50,50)
-                    if norm == 'tmax':
-                        plt.colorbar(tmp,cax = tax, label = 'Counts(t)/max(Counts(t)')
-                    elif norm == 'ptmax':
-                        plt.colorbar(tmp,cax = tax, label = 'Counts(t)/max(Counts(t)')
-                    elif norm == 'max':
-                        plt.colorbar(tmp,cax = tax, label = 'Counts')
-                    elif norm == 'logmax':
-                        plt.colorbar(tmp,cax = tax, label = 'Log10(C)')
-                    elif norm == 'pemax':
-                        plt.colorbar(tmp,cax = tax, label = 'C(E,pixel)/max(C(E,pixel)')
-                    elif norm == 'emax':
-                        plt.colorbar(tmp,cax = tax, label = 'C(E)/max(C(E)')
+            if (norm and i == 0): # or not norm:
+                tax = fig.add_subplot(4,50,50)
+                if norm == 'tmax':
+                    plt.colorbar(tmp,cax = tax, label = 'Counts(t)/max(Counts(t)')
+                elif norm == 'ptmax':
+                    plt.colorbar(tmp,cax = tax, label = 'Counts(t)/max(Counts(t)')
+                elif norm == 'max':
+                    plt.colorbar(tmp,cax = tax, label = 'Counts')
+                elif norm == 'logmax':
+                    plt.colorbar(tmp,cax = tax, label = 'Log10(C)')
+                elif norm == 'pemax':
+                    plt.colorbar(tmp,cax = tax, label = 'C(E,pixel)/max(C(E,pixel)')
+                elif norm == 'emax':
+                    plt.colorbar(tmp,cax = tax, label = 'C(E)/max(C(E)')
 
-                if i == 0:
-                    tl = [l.get_text() for l in ax[i].get_xticklabels()]
-                    ax[i].set_title('Head %i'%head)
-                ax[i].text(0.05,0.05,'%i'%(i),transform=ax[i].transAxes,color = 'k', backgroundcolor = 'w', fontsize = 6)
-                if i == 0:
-                    ax[i].hlines(ebins[8],ptime[0],ptime[-1])
-                    ax[i].hlines(ebins[40],ptime[0],ptime[-1])
-                    ax[i].tick_params(axis='x',labelrotation=45)
-                    ax[i].set_xlabel('Date')
-                    ax[i].set_ylabel('Energy [keV]')
-
-                if i == 1 or i == 2:
-                    if norm == 'tmax':
-                        ax[i].set_ylabel('Counts(t)/max(Counts(t)')
-                    elif norm == 'ptmax':
-                        ax[i].set_ylabel('Counts(t)/max(Counts(t)')
-                    elif norm == 'max':
-                        ax[i].set_ylabel('Counts')
-                    elif norm == 'logmax':
-                        ax[i].set_ylabel('Log10(C)')
-                    elif norm == 'pemax':
-                        ax[i].set_ylabel('C(E,pixel)/max(C(E,pixel)')
-                    elif norm == 'emax':
-                        ax[i].set_ylabel('C(E)/max(C(E)')
-        
+            if i == 0:
+                tl = [l.get_text() for l in ax[i].get_xticklabels()]
+                ax[i].set_title('Head %i'%head)
+            ax[i].text(0.05,0.05,'%i'%(i),transform=ax[i].transAxes,color = 'k', backgroundcolor = 'w', fontsize = 6)
+            if i == 0:
+                ax[i].hlines(ebins[8],ptime[0],ptime[-1])
+                ax[i].hlines(ebins[40],ptime[0],ptime[-1])
+                ax[i].tick_params(axis='x',labelrotation=45)
+                ax[i].set_xlabel('Date')
+                ax[i].set_ylabel('Energy [keV]')
 
             # To Do...
             # Wie sieht Struktur von pdat aus??? Wie genau mache ich die Histogramme??? 
 
             if i == 1:      
                 # Projektion auf Energie-Achse  
-                ax.append(fig.add_subplot(3,1,i))
+                ax.append(fig.add_subplot(3,1,i+1))
                 ax[-1].hist(np.sum(pdat)/len(pdat),bins=ebins)
             
             if i == 2:
                 # Projektion auf Zeit-Achse
-                ax.append(fig.add_subplot(3,1,i))
+                ax.append(fig.add_subplot(3,1,i+1))
                 ax[-1].hist(np.sum(pdat.T)/len(pdat.T),bins=ptime)
+
+            if i == 1 or i == 2:
+                if norm == 'tmax':
+                    ax[i].set_ylabel('Counts(t)/max(Counts(t)')
+                elif norm == 'ptmax':
+                    ax[i].set_ylabel('Counts(t)/max(Counts(t)')
+                elif norm == 'max':
+                    ax[i].set_ylabel('Counts')
+                elif norm == 'logmax':
+                    ax[i].set_ylabel('Log10(C)')
+                elif norm == 'pemax':
+                    ax[i].set_ylabel('C(E,pixel)/max(C(E,pixel)')
+                elif norm == 'emax':
+                    ax[i].set_ylabel('C(E)/max(C(E)')
 
         if period:
             ax[0].set_xlim(period[0],period[1])

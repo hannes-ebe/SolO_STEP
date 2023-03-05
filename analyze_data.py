d1886ae8e4fdd5dd5a7b2800af080e87f93127f1
ebeling <ebeling@physik.uni-kiel.de>
2023-03-05 15:54:21 +0100

    Mal sehen, ob es funktioniert

--- analyze_data.py
+++ analyze_data.py
@@ -8,15 +8,16 @@ from analyze_nom_II import STEP
 
 dat = STEP(2021,12,4)
 dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
+dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/',period=[dt.datetime(2021,12,4,13),dt.datetime(2021,12,4,16)])
 
-dat = STEP(2021,12,5)
-dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
+# dat = STEP(2021,12,5)
+# dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
 
-dat = STEP(2022,1,14)
-dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
+# dat = STEP(2022,1,14)
+# dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
 
-dat = STEP(2022,1,18)
-dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
+# dat = STEP(2022,1,18)
+# dat.marginal_distribution(pixel=4,norm='tmax',save='analyze/')
 
 # Da Funktioniert etwas nicht...
 # dat = STEP(2021,2,7)

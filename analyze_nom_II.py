f24281f1de140e1b63ae5a6f6d1f22898b38d483
ebeling <ebeling@physik.uni-kiel.de>
2023-04-02 16:42:08 +0200

    Hmm

--- analyze_nom_II.py
+++ analyze_nom_II.py
@@ -379,6 +379,52 @@ class STEP:
                 plt.savefig(filename)
         print('Plotted ' + filename + ' successfully.')
 
+    def evolution_energy_means_new(self, filename, ebins=ebins,res = '1min', head = 0, period = None, window_width = 5, save = False, norm = False, overflow = True, esquare = False, box_list = False):
+        '''Übergebe period und Zeitfensterbreite. Intergriere die jeweiligen Energien in den Zeitfenstern und stelle die Entwicklung der means für die einzelnen Pixel da.
+        Bei minütlicher Auflösung entspricht window_width von 5 Einträgen fünf Minuten.'''
+        i = 0
+        pixel_means = [[] for i in range(16)]     # Liste mit Listen der Mittelwerte der einzelnen Pixel. Die erste Liste enthält die Zeitstempel (jeweils Mitte der Zeitfenster)
+        
+        if type(box_list) == list:
+            little_helper = self.data_prep(ebins,res,head,period,norm,overflow,esquare)[0]
+            pldat = self.data_boxes(little_helper,box_list)
+        else:
+            pldat = self.data_prep(ebins,res,head,period,norm,overflow,esquare)[0]
+
+        while (period[0] + dt.timedelta(minutes=(i+1)*window_width)) <= period[1]:
+            window = [period[0] + dt.timedelta(minutes=i*window_width), period[0] + dt.timedelta(minutes=(i+1)*window_width)]
+
+            pixel_means[0].append(period[0] + dt.timedelta(minutes=(i+0.5)*window_width))
+            
+            # Berechnung der Mittelwerte:
+            for k in range(1,16):
+                pdat = pldat[k]
+                integral = np.sum(pdat,axis=0)
+                # calculating mean
+                mean = 0.0
+                for j in range(len(integral)):
+                    # Für Bestimmung des Mittelwertes der Verteilung wird Mitte der Bins gewählt
+                    mean += integral[j]*0.5*(ebins[j+1]+ebins[j])
+                mean = mean/np.sum(integral)
+                pixel_means[k].append(mean)
+            i +=1
+        
+        fig, ax = self.step_plot('Time', 'Mean of energy distribution [keV]', 'Evolution of mean of energy distribution for head ' + str(head) + ' from ' + str(period[0]) + ' to ' + str(period[1]) + ' (' + str(window_width) + ' minute steps)')
+        for i in range(1,16):
+            ax[i].plot(pixel_means[0],pixel_means[i])
+            ax[i].set_yscale('log')
+            if i > 10:
+                ax[i].tick_params(axis='x',labelrotation=90)
+            ax[i].axhline(ebins[8],color='firebrick')
+            ax[i].axhline(ebins[40],color='firebrick') #,label='Energy range of STEP')
+            
+        if save:
+            if type(save) == str:
+                plt.savefig(save + filename)
+            else:
+                plt.savefig(filename)
+        print('Plotted ' + filename + ' successfully.')
+    
     def evolution_energy_means(self, filename, ebins=ebins,res = '1min', head = 0, period = None, window_width = 5, save = False, norm = False, overflow = True, esquare = False, box_list = False):
         '''Übergebe period und Zeitfensterbreite. Intergriere die jeweiligen Energien in den Zeitfenstern und stelle die Entwicklung der means für die einzelnen Pixel da.'''
         i = 0

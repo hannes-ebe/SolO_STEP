78bcbdd6534da28748228f996825ba16c4be232e
ebeling <ebeling@et224.physik.uni-kiel.de>
2023-04-27 13:12:32 +0200

    Wie sind die Winkel definiert???

--- workbench.py
+++ workbench.py
@@ -21,14 +21,54 @@ ebins = np.array([  0.98 ,   2.144,   2.336,   2.544,   2.784,   3.04 ,   3.312,
        137.216, 149.504, 162.816, 178.176, 194.56 , 211.968, 230.4  ,
        372.736])
 
+# Field of View of STEP (Phi, Theta):
+fov = np.array([[-25,24],[-25,12],[-25,0],[-25,-12],[-25,-24],[-35,24],[-35,12],[-35,0],[-35,-12],[-35,-24],[-45,24],[-45,12],[-45,0],[-45,-12],[-45,-24]])
+
 
 
 # Versuch Magnetfelddaten aus cdf zu laden
 
-dat = mag.MAGdata(period=[dt.datetime(2021,12,4),dt.datetime(2021,12,5)])
+# dat = mag.MAGdata(period=[dt.datetime(2021,12,4),dt.datetime(2021,12,5)])
 
-dat.pw_ts()
+# dat.pw_ts()
 
+
+
+
+### Plausibilitätscheck Pitchwinkel-Berechnung ###
+
+def pw1(v_theta,v_phi,B_theta,B_phi):
+    return (np.arccos(np.sin(v_theta)*np.cos(v_phi)*np.sin(B_theta)*np.cos(B_phi) + np.sin(v_theta)*np.sin(v_phi)*np.sin(B_theta)*np.sin(B_phi) + np.cos(v_theta)*np.cos(B_theta)))/np.pi*180
+
+def pw2(v_theta,v_phi,B_theta,B_phi):
+    return (np.arccos(np.sin(v_phi)*np.cos(v_theta)*np.sin(B_phi)*np.cos(B_theta) + np.sin(v_phi)*np.sin(v_theta)*np.sin(B_phi)*np.sin(B_theta) + np.cos(v_phi)*np.cos(B_phi)))/np.pi*180
+
+B_phi = -35
+B_theta = 0
+
+plausibilitätscheck_pw = []
+
+for i in range(len(fov)):
+    plausibilitätscheck_pw.append(pw2(fov[i][1],fov[i][0],B_theta,B_phi))
+    
+print(plausibilitätscheck_pw)
+
+fig, ax = plt.subplots(1,1,figsize=(8,7))
+
+x_corners = [0,1,2,3,4,5]
+y_corners = [0,1,2,3]
+vmin = min(plausibilitätscheck_pw)
+vmax = max(plausibilitätscheck_pw)
+     
+pw_list = np.array([plausibilitätscheck_pw[10:15],plausibilitätscheck_pw[5:10],plausibilitätscheck_pw[0:5]])
+            
+tmp = ax.pcolormesh(x_corners,y_corners,pw_list,vmin=vmin,vmax=vmax)
+plt.colorbar(tmp,label='pitch angle [°]')
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax.set_title('plausibility check of pitch angle calculation')
+
+plt.show()
 
 
 

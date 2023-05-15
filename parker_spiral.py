fb2156ee158714221174a2d4977db5cdb81dca7b
ebeling <ebeling@physik.uni-kiel.de>
2023-05-15 13:19:24 +0200

    Parker-Spirale geht noch immer nicht...

--- parker_spiral.py
+++ parker_spiral.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# astronomical unit  [m]
-au = 1.5e11
-# solar wind speed [m/s]
-v = 500*3.6
-# angular velocity earth [1/s]
-omega = 2*np.pi/86400
+# astronomical unit  [km]
+au = 1.496e8
+# solar wind speed [km/s]
+v = 500/3600
+# angular velocity sun [1/s]
+omega = 2.9e-4 #2*np.pi/27.28/86400 # 2.9e-4
+print(omega)
 
-# number of steps
-steps = 1000
+# step size
+stepsize = 1e6
+
+'''Irgendwas funktioniert mit dem Omega nicht!!!'''
 
 
-
-r = np.linspace(0.0, au, steps)
+r = np.arange(0, 2*au, step=stepsize)
 
 
 # Sonnenwind bewegt sich mit v radial nach außen. Die Zeit berechnet sich als r/v
@@ -22,15 +24,14 @@ t_arrival = au/v
 
 # theta = 2*np.pi*omega*np.linspace(0,t_arrival,steps)
 theta = omega*r/v
-
-print(theta)
-
+# theta = np.degrees(theta)
+print(r)
 
 
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.plot(theta, r)
-ax.set_rmax(2)
-ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
+ax.set_rmax(2*au)
+ax.set_rticks(np.array([0.5, 1, 1.5, 2])*au)  # Less radial ticks
 ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
 ax.grid(True)
 

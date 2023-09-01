1d89c9c6b96c76465e04fcadca3bc0cdff5df75e
ebeling <ebeling@et224.physik.uni-kiel.de>
2023-09-01 13:49:46 +0200

    Interessante Asymmetrie bleibt...

--- total_comparison.py
+++ total_comparison.py
@@ -23,45 +23,118 @@ ebins = np.array([  0.98 ,   2.144,   2.336,   2.544,   2.784,   3.04 ,   3.312,
 
 ### 2021-12-4 ###
 
-# dat = STEP(2021, 12, 4, mag_path = 'default')
-dat = STEP(2021, 12, 4, rpath='data/STEP/', mag_path='data/mag/srf', mag_frame = 'srf')
-period =(dt.datetime(2021,12,4,13,50),dt.datetime(2021,12,4,14,30))
-def grenz(t):
-    return -0.5*t + 20
+# dat = STEP(2021, 12, 4, mag_path = 'default', mag_frame = 'srf')
+# # dat = STEP(2021, 12, 4, rpath='data/STEP/', mag_path='data/mag/srf', mag_frame = 'srf')
+# period =(dt.datetime(2021,12,4,13,50),dt.datetime(2021,12,4,14,30))
+# def grenz(t):
+#     return -0.5*t + 20
 
 
-pixel_means5, pixel_var5 = dat.calc_energy_means(ebins=ebins,head=-1, period=period, grenzfunktion=grenz, norm='ptmax')
-pw5, pw5_time = dat.calc_pw(period, window_width=5)
+### 2022-11-12 ###
+
+dat = STEP(2022, 11, 12, mag_path='default', mag_frame='srf')
+period = (dt.datetime(2022,11,12,2,40),dt.datetime(2022,11,12,3,25))
+grenz = None
 
 
-dat.plot_ts(period=period, head=-1, save='total_comparison/', norm='tmax',grenzfunktion=grenz)
+### 2022-11-19 ###
 
-# for i in range(1,16):
-#     for j in range(1,16):
-#         # Bei gleichen Pixeln dürfte sich nichts ändern...
-#         dat.pixel_comparison_corrected(pixel_means5, pixel_var5, pw5, f'total_comparison/corrected_energies_5_minutes_pixel_{i}_{j}.png', i, j)
-#         plt.close('all')
+# dat = STEP(2022, 11, 19, mag_path = 'default', mag_frame = 'srf')
+# period = (dt.datetime(2022,11,19,14),dt.datetime(2022,11,19,16,15))
+# grenz = None
 
 
 
+def func_total_comparison(dat=dat, period=period, grenz=grenz):
+    pixel_means5, pixel_var5 = dat.calc_energy_means(ebins=ebins,head=-1, period=period, grenzfunktion=grenz, norm='ptmax')
+    pw5, pw5_time = dat.calc_pw(period, window_width=5)
+    year = str(period[0].year - 2000)
+    if period[0].month < 10:
+        month = '0' + str(period[0].month)
+    else:
+        month = str(period[0].month)
+    if period[0].day < 10:
+        day = '0' + str(period[0].day)
+    else:
+        day = str(period[0].day)
+    
+    
+    dat.plot_ts(period=period, head=-1, save='total_comparison/', norm='ptmax',grenzfunktion=grenz)
+    dat.distribution_ring(f'time_series_energy_means_pw_{year}_{month}_{day}','mean of energy (time series)',head=-1,window_width=1,norm='ptmax',save='total_comparison/',period=period,grenzfunktion=grenz,below=True,close=True)
+    print(f'{year}_{month}_{day}')
+    
+    # for i in range(1,16):
+    #     for j in range(1,16):
+    #         # Bei gleichen Pixeln dürfte sich nichts ändern...
+    #         dat.pixel_comparison_corrected(pixel_means5, pixel_var5, pw5, f'total_comparison/corrected_energies_5_minutes_pixel_{i}_{j}.png', i, j)
+    #         plt.close('all')
+    
+    
+    ### Untersuche Pixel 4 etwas genauer ###
+    
+    for j in range(1,16):
+        dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel4_{year}_{month}_{day}/corrected_energies_total_info_pixel_i=4_{j}.png', 4, j)
+        plt.close('all')
+    
+    for i in range(1,16):
+        dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel4_{year}_{month}_{day}/corrected_energies_total_info_pixel_{i}_j=4.png', i, 4)
+        plt.close('all')
+    
+    
+    ### Teste mal Korrektur auf Pixel 11 ###
+    
+    for j in range(1,16):
+        dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel11_{year}_{month}_{day}/corrected_energies_total_info_pixel_i=11_{j}.png', 11, j)
+        plt.close('all')
+    
+    for i in range(1,16):
+        dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel11_{year}_{month}_{day}/corrected_energies_total_info_pixel_{i}_j=11.png', i, 11)
+        plt.close('all')
+        
+        
+def step_plot_correction(dat=dat, period=period, grenz=grenz):
+    pixel_means, pixel_var = dat.calc_energy_means(ebins=ebins,head=-1, period=period, grenzfunktion=grenz, norm='ptmax')
+    pw, pw_time = dat.calc_pw(period, window_width=5)
+    year = str(period[0].year - 2000)
+    if period[0].month < 10:
+        month = '0' + str(period[0].month)
+    else:
+        month = str(period[0].month)
+    if period[0].day < 10:
+        day = '0' + str(period[0].day)
+    else:
+        day = str(period[0].day)
+    
+    fig, ax = dat.step_plot('time', 'mean of energy [keV]', 'energy means with pitch angle correction')
+    
+    pixel1 = 4
+    for pixel2 in range(1,16):
+        ax[pixel2].errorbar(pixel_means[0],pixel_means[pixel1],yerr=np.sqrt(pixel_var[pixel1]),marker='x',label=f'mean pixel {pixel1}')
+            
+        # Übergebe willkürliche Fehler, da ich diese eh nicht brauche.
+        energy2_corrected = dat.energy_correction(pixel_means[pixel2],pw[pixel1-1],pw[pixel2-1],2,2)[0]
+            
+        ax[pixel2].errorbar(pixel_means[0],energy2_corrected,yerr=np.sqrt(pixel_var[pixel2]),marker='x',label=f'mean pixel {pixel2}')
+        ax[pixel2].tick_params(axis='x',labelrotation=45)
+        ax[pixel2].legend()
+    plt.savefig(f'total_comparison/step_plot_total_correction_energy_means_pixel{pixel1}_{year}_{month}_{day}.png')
+    plt.close('all')
+    
 
-### Untersuche Pixel 4 etwas genauer ###
-
-for j in range(1,16):
-    dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel4_21_12_04/corrected_energies_total_info_pixel_i=4_{j}.png', 4, j)
+    fig, ax = dat.step_plot('time', 'difference of energy means [keV]', 'difference of energy means with correction')
+        
+    for pixel2 in range(1,16):
+        # Übergebe willkürliche Fehler, da ich diese eh nicht brauche.
+        energy2_corrected = dat.energy_correction(pixel_means[pixel2],pw[pixel1-1],pw[pixel2-1],2,2)[0]
+        diff_corrected = energy2_corrected - pixel_means[pixel1]
+        
+        ax[pixel2].plot(pixel_means[0],diff_corrected,marker='x')
+        ax[pixel2].axhline(0,color='tab:red')
+            
+        ax[pixel2].tick_params(axis='x',labelrotation=45)
+    plt.savefig(f'total_comparison/step_plot_total_correction_differences_energy_pixel{pixel1}_{year}_{month}_{day}.png')
     plt.close('all')
 
-for i in range(1,16):
-    dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel4_21_12_04/corrected_energies_total_info_pixel_{i}_j=4.png', i, 4)
-    plt.close('all')
 
-
-### Teste mal Korrektur auf Pixel 11 ###
-
-for j in range(1,16):
-    dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel11_21_12_04/corrected_energies_total_info_pixel_i=11_{j}.png', 11, j)
-    plt.close('all')
-
-for i in range(1,16):
-    dat.pixel_comparison_total(pixel_means5, pixel_var5, pw5, pw5_time, f'total_comparison/pixel11_21_12_04/corrected_energies_total_info_pixel_{i}_j=11.png', i, 11)
-    plt.close('all')
+func_total_comparison(dat,period,grenz)
+step_plot_correction(dat,period,grenz)
